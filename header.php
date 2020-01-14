44026869dd6305a9162449054b0955823fdb2ed1
yoan parede <parede.yoan.amcs@gmail.com>
2020-01-14 13:13:51 +0100

    create Header.php

--- header.php
+++ header.php
@@ -6,5 +6,8 @@
             <link rel="stylesheet" type="text/css" href="css/style.css">
             <meta charset="utf-8">
         </head>
+        <header>
+            <h1>ohmyfood</h1>
+        </header>
     </body>
 </html>
