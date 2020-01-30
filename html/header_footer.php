0f75af921384440c38391e8f288ac34820e8b0c8
yoan parede <parede.yoan.amcs@gmail.com>
2020-01-30 16:03:59 +0100

    creater file in .php test

---
+++ html/header_footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    
+    <a class="hidden" href="index.html">   
+        <header>
+                <h1>ohmyfood</h1>
+        </header>
+    </a>
+
+
+    <footer>
+
+        <a class="effect_wiggle mentions_legales" href="#">Mentions légales</a>
+        <a class="effect_wiggle contactez-nous" href="mailto:parede.yoan.amcs@gmail.com">Contactez-nous</a>
+    </footer>
+</html>
