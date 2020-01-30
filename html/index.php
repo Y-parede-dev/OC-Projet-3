d13dcfc21c82520a1e9506fef47079d36cd3e9ca
yoan parede <parede.yoan.amcs@gmail.com>
2020-01-30 16:09:41 +0100

    add idex.php

---
+++ html/index.php
@@ -0,0 +1,46 @@
+
+<body>
+    <a class="hidden" href="index.html">
+        <header>
+            <h1>ohmyfood</h1>
+        </header>
+    </a>
+    <section class="container_menu">
+        <a href="menu_top_left.html" class="menu menu_top_left">
+            <div class="anim_menu_zoom">
+                <h2 class="title_menu_all_capitalize title_menu title_menu_top_left">menu</h2>
+                <p class="p_menu p_top_left">la note enchantée</p>
+                <img class ="img_menu" src="../images/note_musique-m.png" alt="image note de musique">
+            </div>
+        </a>
+        
+       <a href="menu_top_right.html" class="menu menu_top_right">
+           <div class="anim_menu_zoom">
+                <h2 class="title_menu title_menu_top_right">menu</h2>
+                <p class="p_menu p_top_right">le chic à la française</p>
+            </div>
+        </a>
+    </section>
+
+    <section class="container_menu">
+        <a href="menu_bottom_left.html" class="menu menu_bottom_left">
+            <div class="anim_menu_zoom">
+                <h2 class="title_menu title_menu_bottom_left">menu</h2>
+                <p class="p_menu p_bottom_left">le délice des papilles</p>
+            </div>
+        </a>
+        <a href="menu_bottom_right.html" class="menu menu_bottom_right">
+            <div class="anim_menu_zoom">
+                <h2 class="title_menu title_menu_bottom_right">menu</h2>
+                <p class="p_menu p_bottom_right">la palette du goût</p>
+            </div>
+        </a>
+    </section>
+    <section class="container">
+        <p>prochainement</p>
+    </section>
+    <footer>
+        <?php include("footer.php"); ?>
+    </footer>
+</body>
+</html>
